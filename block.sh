e2cd45eaf32df9e4e96d0c5e3c93bbe208b6fe29
wexa <shehabhos6@gmail.com>
2025-08-06 00:23:37 +0300

    Initial commit: Add domain blocking script

---
+++ block.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+read -p "Enter a domain name: " domain
+
+ip=$(dig +short "$domain" | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' | head -n 1)
+
+echo "IP address of $domain is: $ip"
+
+# Block the ip
+echo "Blocking $ip using iptables..."
+sudo iptables -A OUTPUT -p icmp -d "$ip" -j DROP
+sudo iptables -A INPUT -p icmp -s "$ip" -j DROP
